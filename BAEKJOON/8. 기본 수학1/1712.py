351bb62b0cfb13685412535953fb1d5f5b9b67eb
pkyh2 <pkyh1@naver.com>
2021-11-13 23:54:58 +0900

    step8 2(ing)

--- BAEKJOON/8. 기본 수학1/1712.py
+++ BAEKJOON/8. 기본 수학1/1712.py
@@ -1,16 +1,29 @@
+# 손익분기점
+
+# 자꾸 시간초과 발생
+'''
 A, B, C = map(int, input().split())
 
 production = A + B
 sale = C
 cnt = 1
-while True:
-    if B > C:
-        print(-1)
-        break
-    if production >= sale:
+
+if B >= C:
+    print(-1)
+else:
+    while production >= sale:
         sale += C
         production += B
         cnt += 1
-    else:
-        print(cnt)
-        break
+    print(cnt)
+'''
+
+# 수식을 작성해서 해결
+A, B, C = map(int, input().split())
+ 
+if B >= C:
+    print(-1)
+else:
+    N = A / (C - B)
+    N = N + 1
+    print(int(N))
