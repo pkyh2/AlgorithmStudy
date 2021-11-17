c9a6c3cd9dac6169cd8cb89ba1cacb83102b5da8
pkyh2 <pkyh1@naver.com>
2021-11-17 23:05:46 +0900

    step8 5

--- BAEKJOON/8. 기본 수학1/05_10250.py
+++ BAEKJOON/8. 기본 수학1/05_10250.py
@@ -5,20 +5,27 @@
 선호하는 가까운 거리의 방은 낮은 층의 1번째 방을 선호한다. ex) 102호 < 301호
 '''
 
-# 풀이 1
+# 풀이 1(반복문)
 '''
 1. W, H 값을 입력받고 호텔의 방 호수를 선호 순서대로 리스트에 저장
 2. N번째 해당하는 원소값을 출력
 '''
-#T = int(input())
+T = int(input())
 
-#for _ in range(T):
-H, W, N = map(int, input().split())
-hotelRoom = []
-for i in range(1, W+1):
-    for j in range(1, H+1):
+for _ in range(T):
+    H, W, N = map(int, input().split())
+    hotelRoom = []
+    for i in range(1, W+1):
+        if i < 10:
+            i = '0' + str(i)
+        for j in range(1, H+1):
+            hotelRoom.append(str(j) + str(i))
+    
+    print(hotelRoom[N-1])
 
-        hotelRoom.append(str(j) + str(i))
-
-print(hotelRoom)
-
+# 풀이 2(일반식)
+'''
+1. 
+'''
+a = int(N/H)
+b = N%H
