4c87416b3e831ca11d6df5d764914f03690d3cfb
pkyh2 <pkyh1@naver.com>
2021-11-25 21:30:38 +0900

    step9 3

--- BAEKJOON/9. 기본 수학2/03_11653.py
+++ BAEKJOON/9. 기본 수학2/03_11653.py
@@ -4,21 +4,31 @@
 정수 N이 주어졌을때, 소인수 분해하는 프로그램을 작성하시오.
 '''
 
-# 풀이 1
+# 풀이 1 (재귀함수)
 # 1. 정수 N을 입력받는다.
-# 2. N이 1이면 걍 종료
-# 3. N > 1면 2 ~ int(N/2)+1 까지 반복
-# 4. N과 최소로 나눠지는 수로 나누고 몫을 다시 N에 저장
-# 5. 나눠지는 수를 리스트에 저장
-# 6. N이 소수면 그냥 N을 출력
+# 2. temp에 N을 대입하고 temp가 1이면 걍 종료
+# 3. temp != 1면 range(2, int(temp/2)+1) 까지 반복 -> 마지막 소인수가 출력이 안되서 range(2, temp+1)까지로 변경
+# 4. temp과 최소로 나눠지는 수를 출력하고
+# 5. 그 나눠지는 수로 temp //= i해서 temp에 다시 저장
+# 6. 함수 반복
 
 N = int(input())
 temp = N
-if temp == 1:
-    print('')
 
-minimumPrimeNum = []
-for i in range(2, int(temp/2)+1):
-    if temp % i == 0:
-        minimumPrimeNum.append(i)
-        if len(minimumPrimeNum) > 1     # 리스트에 값이 들어가면 들어간 값으로 N을 나눠준 몫을 다시 계산한다.
+def primeFactorization(temp):
+    if temp != 1:
+        if temp <= 3:
+            print(temp)
+        else:
+            for i in range(2, temp+1):
+                if temp % i == 0:
+                    print(i)
+                    temp //= i
+                    return primeFactorization(temp)
+
+    elif temp == 1:
+        return
+
+primeFactorization(temp)
+
+# 시간초가 좀 걸린다...
