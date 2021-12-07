a45ff4d393150d1efb42ee7e916dde6a7db290f6
pkyh2 <pkyh1@naver.com>
2021-12-07 22:25:25 +0900

    step11 01

---
+++ BAEKJOON/11. 브루트 포스/01_2798.py
@@ -0,0 +1,74 @@
+# 블랙잭
+'''
+N 장의 카드를 모두 보이도록 바닥에 놓는다.
+딜러가 숫자 M을 크게 외친다.
+N장 중 3장의 카드를 골라야 한다.
+3장의 카드 합이 M을 넘지않는 최대한 가까운 수를 출력하시오.
+'''
+
+# 풀이1 for문
+# 1, 2, 3
+# 1, 2, 4
+# 1, 2, 5 ...
+
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+result = []
+
+for i in range(len(cards)):
+    for j in range(1, len(cards)):
+        for k in range(2, len(cards)):
+            if i < j < k:       # 핵심 키워드!
+                cardsSum = cards[i]+cards[j]+cards[k]
+                if cardsSum <= M:
+                    result.append(cardsSum)
+
+print(max(result))
+#35316KB 208ms
+
+# 풀이2 while문
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+result = []
+
+i = 0
+while i < len(cards):
+    j = i+1
+    while j < len(cards):
+        k = j+1
+        while k < len(cards):
+            if i < j < k:       #핵심 키워드!
+                cardsSum = cards[i]+cards[j]+cards[k]
+                if cardsSum <= M:
+                    result.append(cardsSum)
+            k += 1
+        j += 1
+    i += 1
+
+print(max(result))
+# 35316KB 152ms
+
+## 시간 줄이는 법
+# set함수 사용
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+result = set()                  # 합계에 중복이 있을 수 있으니까
+
+i = 0
+while i < len(cards):
+    j = i+1
+    while j < len(cards):
+        k = j+1
+        while k < len(cards):
+            if i < j < k:       #핵심 키워드!
+                cardsSum = cards[i]+cards[j]+cards[k]
+                if cardsSum <= M:
+                    result.add(cardsSum)
+            k += 1
+        j += 1
+    i += 1
+
+print(max(result))
