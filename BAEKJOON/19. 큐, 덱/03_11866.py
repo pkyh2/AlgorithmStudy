659c56aecbc65236c010acae2b73a869bbb1480c
pkyh2 <pkyh1@naver.com>
2022-01-09 17:29:38 +0900

    step19 03(ing)

--- BAEKJOON/19. 큐, 덱/03_11866.py
+++ BAEKJOON/19. 큐, 덱/03_11866.py
@@ -10,6 +10,37 @@
 # K번만큼 반복하면서 k-1번까지는 수는 다른리스트에 저장하고 k번째 수는 결과값에 저장
 # 기존 리스트에 앞에서 저장한 리스트를 붙이고 다시 반복
 
+# import sys
+# from collections import deque
+
+# input = sys.stdin.readline
+
+# N, K = map(int, input().split())
+# circularQueue = deque(list(range(1, N+1)))
+# result = []
+
+# while len(circularQueue) > 1:
+#     tempQueue = deque([])
+#     if len(circularQueue) < K:
+#         if len(circularQueue) > int(K/2):
+#             for i in range(int(K/2)):
+#                 result.append(circularQueue.pop(i))
+    
+#     for i in range(K):
+#         if i == K-1:
+#             result.append(circularQueue.popleft())
+#         else:
+#             tempQueue.append(circularQueue.popleft())
+
+#     circularQueue += tempQueue
+#     print(circularQueue)
+
+
+# print(circularQueue)
+# print(result)
+
+
+# 풀이2(rotate)
 import sys
 from collections import deque
 
@@ -19,20 +50,28 @@ N, K = map(int, input().split())
 circularQueue = deque(list(range(1, N+1)))
 result = []
 
-while len(circularQueue) > 1:
+while len(circularQueue) >= 1:
     tempQueue = deque([])
-    for i in range(K):
-        if len(circularQueue) < K:
-            if K % 2 == 0:
-                pass
-        if i == K-1:
-            result.append(circularQueue.popleft())
+    if len(circularQueue) < K:
+        if K % len(circularQueue) == 0:
+            result.append(circularQueue.pop())
         else:
-            tempQueue.append(circularQueue.popleft())
-
-    circularQueue += tempQueue
+            if K%len(circularQueue)-1 != 0:
+                result.append(circularQueue[K%len(circularQueue)-1])
+                circularQueue.remove(circularQueue[K%len(circularQueue)-1])
+                circularQueue.rotate(-(K//len(circularQueue)))
+            else:
+                result.append(circularQueue[K%len(circularQueue)-1])
+                circularQueue.remove(circularQueue[K%len(circularQueue)-1])
+    else:
+        for i in range(K):
+            if i == K-1:
+                result.append(circularQueue.popleft())
+            else:
+                circularQueue.rotate(-1)
     print(circularQueue)
 
-
-print(circularQueue)
-print(result)
+print('<', end='')
+for i in range(len(result)-1):
+    print(result[i], end=', ')
+print('{}>'.format(result[-1]))
