79f14c69deab9753166c758a1294175eabc6534e
pkyh2 <pkyh1@naver.com>
2022-01-10 23:15:51 +0900

    step19 05

--- BAEKJOON/19. 큐, 덱/05_1021.py
+++ BAEKJOON/19. 큐, 덱/05_1021.py
@@ -5,27 +5,32 @@
 '''
 
 # 풀이1
-# 1 ~ N까지 덱을 만든다.
-# 찾으려고 하는 인덱스가 N의 절반이상이면
-# 뒤에서부터 값을 앞으로 옮기면서 cnt를 증가시킨다.
-# 그렇지 않으면 앞에서 뒤로 옮기면서 cnt를 증가시킨다.
-# 원하는 숫자를 찾으면 제거한다.
-# 원하는 숫자의 인덱스를 찾는다 그게 절반 이상이면 뒤에서 찾고 아니면 밑에서 찾는다.
-
+# 뽑는 수를 순서대로 뽑으면서
+# 큐의 맨앞의 숫자와 똑같으면 제거하고
+# 그렇지 않으면 뒤로 보낸다.
+# 뽑는 수가 전체 큐 길이의 절반 이상에 위치하면 앞으로 보낸다.
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-position = list(map(int, input().split()))  # 2 9 5
+position = deque(list(map(int, input().split())))  # 2 9 5
 
-deque = deque(list(range(1, N+1)))  # 1 2 3 4 5 6 7 8 9 10
+dq = deque(list(range(1, N+1)))  # 1 2 3 4 5 6 7 8 9 10
 cnt = 0
-while len(position):
-    i = 0
-    if position[i] == deque[i]:
-        if i <= len(deque) // 2:
-            deque.popleft()
+for i in position:
+    while True:
+        if i == dq[0]:
+            dq.popleft()
+            break
         else:
-            deque.pop()
+            if dq.index(i) > len(dq)//2:
+                dq.rotate(1)
+                cnt += 1
+            else:
+                dq.rotate(-1)
+                cnt += 1
+
+print(cnt)
+# 32368 92
