da4320396d1b36769d2340cdc44dd131344aab5f
pkyh2 <pkyh1@naver.com>
2022-02-16 23:57:15 +0900

    step14 01,02

--- BAEKJOON/14. 백트래킹/01_15649.py
+++ BAEKJOON/14. 백트래킹/01_15649.py
@@ -22,3 +22,8 @@ dfs()
 # 30864 216
 
 # 순열함수
+from itertools import permutations
+N, M = map(int, input().split())
+P = permutations(range(1, N+1), M)
+for i in P:
+    print(' '.join(map(str, i)))
--- BAEKJOON/14. 백트래킹/02_15650.py
+++ BAEKJOON/14. 백트래킹/02_15650.py
@@ -18,3 +18,10 @@ def dfs(start):
 
 dfs(1)
 # 30864 64
+
+# 조합
+from itertools import combinations
+N, M = map(int, input().split())
+C = combinations(range(1, N+1), M)
+for i in C:
+    print(' '.join(map(str, i)))
