b287b494e07489c5e3c20c4aeb3e6dcc52ff7ee4
pkyh2 <pkyh1@naver.com>
2022-04-06 00:15:51 +0900

    level1(ing)

--- Programmers/05_기능개발.py
+++ Programmers/05_기능개발.py
@@ -1,37 +1,21 @@
-# level2
-def deployFunc(deploy, answer):
-    cnt = 0
-    if len(deploy) > 1:
-        for i in range(1, len(deploy)):
-            if deploy[0] >= deploy[i]:
-                cnt += 1
-            else:
-                deploy = deploy[i:]
-                break
-        print(deploy)
-    else:
-        return
-    answer.append(cnt) 
-    return deployFunc(deploy, answer)
-
 def solution(progresses, speeds):
     answer = []
-    deploy = []
-    for i in range(len(progresses)):
-        deploy.append((100 - progresses[i])/speeds[i])
 
-    deployFunc(deploy, answer)
-    # 100 - 93 / 1 == 7
-    # 100 - 30 / 30 == 2.333
-    # 100 - 55 / 4 == 9
-    
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
 
+        cnt = 0
+        while progresses and progresses[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        
+        if cnt > 0:
+            answer.append(cnt)
     
     return answer
 
 progresses = [93, 30, 55]
 speeds = [1, 30, 5]
-print(solution(progresses, speeds))
-progresses = [95, 90, 99, 99, 80, 99]
-speeds = [1, 1, 1, 1, 1, 1]
 print(solution(progresses, speeds))
