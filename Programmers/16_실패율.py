35ef9a8e4bcb3d3e35dde84818d9d0c42d6d1aeb
pkyh2 <pkyh1@naver.com>
2022-05-10 23:57:56 +0900

    level1

--- Programmers/16_실패율.py
+++ Programmers/16_실패율.py
@@ -1,17 +1,25 @@
 def solution(N, stages):
+    # 1부터 N까지 key값으로하고 value가 0인 dict 초기화
     clear = { key:0 for key in range(1, N+1) }
+    # 시간복잡도 O(n**2) 각 stage별 clear수 체크
     for i in stages:
         for j in clear.keys():
             if i == j:
                 clear[j] += 1
+    # dict -> list, tuple 형태로 변경
     clear = sorted(clear.items(), key=lambda x: x[0])
 
     fail = {}
     challenger = len(stages)    # 8
+    # 각 stage별 실패율 계산
     for i in clear:
-        fail[i[0]] = i[1]/challenger
+        if challenger == 0:
+            fail[i[0]] = 0
+        else:
+            fail[i[0]] = i[1]/challenger
         challenger -= i[1]
 
+    # fail dict에서 value별로 정렬하고, 해당 key값만 list로 반환
     fail = list(map(lambda x: x[0], sorted(fail.items(), key = lambda x: float(x[1]), reverse=True)))
     
     return fail
