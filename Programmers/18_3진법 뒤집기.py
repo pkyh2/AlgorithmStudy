30db8cf75350e0e09954633364ea47dba97cff18
pkyh2 <pkyh1@naver.com>
2022-05-16 22:34:02 +0900

    level1

--- Programmers/18_3진법 뒤집기.py
+++ Programmers/18_3진법 뒤집기.py
@@ -4,8 +4,9 @@ def solution(n):
     while n > 0:
         ternary.append(n % 3)
         n //= 3
-    ternary = int(''.join(map(str, ternary)))
-    answer = int(ternary, 3) 
+    ternary = ''.join(map(str, ternary))
+    for i in range(len(ternary)):
+        answer += int(ternary[len(ternary)-i-1]) * (3**i)
     return answer
 
 n = 45
